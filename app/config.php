7933903ef3189685b11376f62b585390464ec360
Bruno Campos Santos <contato@hxtutors.com.br>
2015-10-22 11:21:18 -0200

    Configuração do HXPHP Framework

--- app/config.php
+++ app/config.php
@@ -2,52 +2,15 @@
 	//Constantes
 	$configs = new HXPHP\System\Configs\Config;
 
-	/*
-		//Globais
-		$configs->global->models->directory = APP_PATH . 'models' . DS;
+	$configs->env->add('development');
 
-		$configs->global->views->directory = APP_PATH . 'views' . DS;
-		$configs->global->views->extension = '.phtml';
+	$configs->env->development->baseURI = '/sistema/';
 
-		$configs->global->controllers->directory = APP_PATH . 'controllers' . DS;
-		$configs->global->controllers->notFound = 'Error404Controller';
-
-		$configs->title = 'Titulo customizado';
-
-		//Configurações de Ambiente - Desenvolvimento
-		$configs->env->add('development');
-
-		$configs->env->development->baseURI = '/hxphp/';
-
-		$configs->env->development->database->setConnectionData(array(
-			'host' => 'localhost',
-			'user' => 'root',
-			'password' => '',
-			'dbname' => 'hxphp'
-		));
-
-		$configs->env->development->mail->setFrom(array(
-			'from' => 'Remetente',
-			'from_mail' => 'email@remetente.com.br'
-		));
-
-		//Configurações de Ambiente - Produção
-		$configs->env->add('production');
-
-		$configs->env->production->baseURI = '/';
-
-		$configs->env->production->database->setConnectionData(array(
-			'host' => 'localhost',
-			'user' => 'usuariodobanco',
-			'password' => 'senhadobanco',
-			'dbname' => 'hxphp'
-		));
-
-		$configs->env->production->mail->setFrom(array(
-			'from' => 'Remetente',
-			'from_mail' => 'email@remetente.com.br'
-		));
-	*/
-	 
+	$configs->env->development->database->setConnectionData(array(
+		'host' => 'localhost',
+		'user' => 'root',
+		'password' => '',
+		'dbname' => 'sistemahx'
+	));
 
 	return $configs;
